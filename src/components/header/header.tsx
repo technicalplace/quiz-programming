828f6c531e526ccefbf6954760b0ebd15737be28
Shunsuke Fujinawa <shun26497204@gmail.com>
2023-05-25 17:58:59 +0900

    :memo: headerのtitle修正

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './header.css';
 
-export const Header = () => {
+export const Header = (props: {title: string}) => {
+  const { title } = props;
+  
   return (
     <>
       <header className='header'>
         <div className='header__container'>
-          <h2>{}</h2>
+          <h2 className='header__title'>{title}</h2>
           <nav className='header__nav'>
             <ul className='header__ul'>
               <li>Let's Think!</li>
